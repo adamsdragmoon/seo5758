7e4f72741b9a2c901c553b839556bae6f66a0723
Cyberwave <cyberwave78@gmail.com>
2024-02-04 23:34:01 +0700

    initial seo template

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Slot Gacor',
+    default:
+      'Slot Gacor - Pocari4D, Slot Online, Slot Server Thailand, Slot Terpercaya',
+  },
+  description:
+    'Temukan informasi terbaru tentang slot gacor, slot online terpercaya tahun 2024 hanya di website slot gacor',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="id" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
